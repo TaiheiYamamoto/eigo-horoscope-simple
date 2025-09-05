fbbbded2e23a7ef012fa6536ca4aa54329f72f05
TaiheiYamamoto <yamamototaihei55836317@gmail.com>
2025-09-05 12:24:16 +0900

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function Page() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui, Arial" }}>
+      <h1>Next.js App Router: Hello</h1>
+      <p>このページが出れば layout.tsx も認識できています。</p>
+    </main>
+  );
+}
