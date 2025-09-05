c400ebf37e3c4da8f35c944cfa675db293ee0376
TaiheiYamamoto <yamamototaihei55836317@gmail.com>
2025-09-05 12:43:51 +0900

    Delete app/app/page.tsx

--- app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-export default function Page() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui, Arial" }}>
-      <h1>Next.js App Router: Hello</h1>
-      <p>このページが出れば layout.tsx も認識できています。</p>
-    </main>
-  );
-}
